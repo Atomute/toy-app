4f430f91f58a298ded720b8a4bfd1e863cb74586
Atomute <73423300+Atomute@users.noreply.github.com>
2023-08-20 05:28:10 +0000

    All Done

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,9 @@
+# app/models/micropost.rb
+
 class Micropost < ApplicationRecord
     belongs_to :user
     validates :content, length: { maximum: 140 },
                         presence: true
 end
+
+
